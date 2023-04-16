e928efe8ff1e4294b4486bedce41728ecea66458
Johannes <hello@thejoltjoker.com>
2023-04-16 20:50:53 +0200

    move config

---
+++ chatgpt4maya/config.py
@@ -0,0 +1,184 @@
+import shutil
+import tempfile
+import configparser
+import os
+import sys
+from pathlib import Path
+import logging
+import os
+import venv
+import subprocess
+
+from chatgpt4maya import helpers
+
+# Load .env during development
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+MENU = 'ChatGPTMenu'
+BOT_USER = 'Chat GPT'
+REPO_PATH = Path(__file__).parent / '..'
+DATA_PATH = REPO_PATH / 'chatgpt4maya' / 'src'
+CONFIG_PATH = Path.home() / '.ChatGPTForMaya'
+LOG_FORMAT = '%(asctime)s %(module)s %(lineno)d [ChatGPT]: %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+
+def maya_script_path():
+    paths = []
+    environment = os.getenv('MAYA_SCRIPT_PATH')
+    if environment:
+        if sys.platform == 'win32':
+            paths = environment.split(';')
+        else:
+            paths = environment.split(':')
+    return paths
+
+
+def config_path():
+    output = CONFIG_PATH
+    paths = maya_script_path()
+    if paths:
+        user_paths = [x for x in sorted(paths, key=len) if x.startswith(str(Path().home().as_posix()))]
+        if user_paths:
+            output = Path(paths[0]) / 'ChatGPTForMaya'
+    return output
+
+
+class Config:
+    def __init__(self, path=None):
+        """
+        Initialize a new Config object with the given path to the configuration file.
+        If no path is provided, the default path is used.
+        If the configuration file does not exist, create it with some default values.
+
+        Args:
+            path (pathlib.Path, optional): Path to the configuration file. Defaults to None.
+        """
+        self.path = path if path else CONFIG_PATH / 'config.ini'
+        self.parser = configparser.ConfigParser()
+        self.parser.optionxform = str
+
+        # If the configuration file does not exist, create it with some default values
+        if not self.path.is_file():
+            self.parser['OpenAI'] = {'OpenAIApiKey': '',
+                                     'OpenAILibraryPath': ''}
+            if not self.path.parent.is_dir():
+                self.path.parent.mkdir(exist_ok=True, parents=True)
+            with self.path.open('w') as configfile:
+                self.parser.write(configfile)
+
+        # Setup openai api key
+        if not self.get('OpenAI', 'OpenAIApiKey') and os.getenv('OPENAI_API_KEY'):
+            self.set('OpenAI', 'OpenAIApiKey', os.getenv('OPENAI_API_KEY'))
+
+        # Setup openai library path
+        openai_txt_path = DATA_PATH / 'openai.txt'
+        if not self.get('OpenAI', 'OpenAILibraryPath') and openai_txt_path.is_file():
+            openai_pip_path = helpers.pip_location_to_path(openai_txt_path.read_text())
+            library_path = str(openai_pip_path.resolve())
+            self.set('OpenAI', 'OpenAILibraryPath', library_path)
+
+    def _config_path_string(self):
+        return str(self.path.resolve())
+
+    def _read(self):
+        # Read the configuration file
+        self.parser.read(self._config_path_string())
+        return self.parser
+
+    def _write(self):
+        with self.path.open('w') as configfile:
+            self.parser.write(configfile)
+
+    def get(self, section, key, fallback=None):
+        """
+        Read a value from the configuration file for the given key.
+
+        Args:
+            key (str): The key to read from the configuration file.
+
+        Returns:
+            str: The value associated with the key, or None if the key is not found.
+        """
+        self._read()
+        value = fallback
+        if self.parser.has_section(section):
+            value = self.parser[section].get(key)
+        else:
+            logging.warning(f'Section "{section}" does not exist in {self.path.name}')
+        return value
+
+    def get_all(self):
+        self._read()
+        output = {}
+        all_sections = self.parser.sections()
+        all_sections.append('DEFAULT')
+        for each_section in self.parser.sections():
+            output[each_section] = {}
+            for (each_key, each_val) in self.parser.items(each_section):
+                output[each_section][each_key] = each_val
+
+        return output
+
+    def set(self, section, key, value):
+        """
+        Write a key-value pair to the configuration file.
+
+        Args:
+            key (str): The key to write to the configuration file.
+            value (str): The value to associate with the key.
+        """
+        self._read()
+        if not self.parser.has_section(section):
+            self.parser[section] = {key: value}
+        else:
+            # Write the key-value pair to the configuration file
+            self.parser[section][key] = value
+        self._write()
+        return self.parser
+
+
+def setup_openai():
+    # Set up a virtual environment using venv
+    env_dir = CONFIG_PATH / 'venv'
+    env_dir.mkdir(exist_ok=True, parents=True)
+    venv.create(env_dir, with_pip=True)
+
+    # where requirements.txt is in same dir as this script
+    subprocess.check_call([env_dir / 'bin/pip', "install", "openai"], cwd=dir)
+
+    # Return path to virtual environment site-packages
+    return os.path.join(env_dir, 'lib', 'python3.7', 'site-packages')
+
+
+# def setup_openai():
+#     temp_path = Path(tempfile.gettempdir())
+#     url = 'https://github.com/openai/openai-python/archive/refs/heads/main.zip'
+#     zip_file = download_file(url, temp_path / 'openai.zip')
+#     unzipped = unzip(zip_file, temp_path / 'openai-repo')
+#     config = Config()
+#     # Copy files to mayascriptpath
+#     for root, dirs, files in os.walk(unzipped):
+#         root_path = Path(root)
+#         if root_path.name == 'openai':
+#             destination = Path(maya_script_path()[0] if maya_script_path() else REPO_PATH) / 'openai'
+#             if not destination.is_dir():
+#                 shutil.copytree(root_path, destination)
+#                 config.set('OpenAI', 'OpenAIPackagePath', str(destination.parent.resolve().as_posix()))
+#                 logging.info(f'Downloaded and extracted openai package to {destination.resolve()}')
+#                 return True
+#             else:
+#                 logging.warning(f"There's already a folder called openai in {destination.parent.resolve()}")
+#                 return False
+#     logging.warning('Failed to download and extract openai')
+#     return False
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+    setup_openai()
---
+++ chatgpt4maya/helpers.py
@@ -0,0 +1,213 @@
+import random
+import re
+import string
+import urllib.request
+import webbrowser
+import zipfile
+from pathlib import Path
+
+
+def placeholder_text():
+    """Returns a random placeholder text from a list of options"""
+    placeholders = ["Create a new polygon object", "Rename a selected object",
+                    "Move an object to a specified location", "Scale an object to a specified size",
+                    "Rotate an object to a specified angle", "Set the visibility of an object",
+                    "Create a new camera", "Set the camera's position and rotation", "Create a new light",
+                    "Set the light's intensity and color", "Parent one object to another", "Create a new group",
+                    "Set the group's pivot point", "Create a new material", "Assign a material to an object",
+                    "Create a new texture", "Assign a texture to a material",
+                    "Create a new keyframe for an object's animation", "Move an object along a path",
+                    "Create a new particle system", "Set the particle system's properties",
+                    "Create a new constraint", "Set the constraint's target object", "Set the constraint's weight",
+                    "Save the current scene to a file",
+                    "Create a new polygon object and add it to the scene",
+                    "Rename a selected object and change its display name in the viewport",
+                    "Move an object to a specified location and set its pivot point",
+                    "Scale an object to a specified size and uniformly scale its UVs",
+                    "Rotate an object to a specified angle and align it to a surface",
+                    "Set the visibility of an object and keyframe its visibility over time",
+                    "Create a new camera and set its resolution gate",
+                    "Set the camera's position and rotation and keyframe its animation",
+                    "Create a new light and set its type and attributes",
+                    "Set the light's intensity and color and keyframe its animation",
+                    "Parent one object to another and set its local transformation",
+                    "Create a new group and add objects to it",
+                    "Set the group's pivot point and its transformation",
+                    "Create a new material and set its attributes",
+                    "Assign a material to an object and adjust its texture coordinates",
+                    "Create a new texture and set its properties",
+                    "Assign a texture to a material and adjust its UV mapping",
+                    "Create a new keyframe for an object's animation and set its interpolation",
+                    "Move an object along a path and adjust its tangents",
+                    "Create a new particle system and set its emitter",
+                    "Set the particle system's properties and adjust its attributes",
+                    "Create a new constraint and set its target object",
+                    "Set the constraint's weight and keyframe it over time",
+                    "Save the current scene to a file and export selected objects to a new file"]
+    return random.choice(placeholders)
+
+
+def random_bot_handle() -> str:
+    """
+    Generates a random bot handle from a list of pre-defined bot names.
+
+    Returns:
+        str: A randomly selected bot name.
+
+    """
+    bot_handles = [
+        "BotMaster3000",
+        "RoboPal42",
+        "AI_Assistant2",
+        "AutoBotX99",
+        "CyberHelper100",
+        "TechBot3600",
+        "GeniusBotX7",
+        "SmartAI_Helper21",
+        "Botitude3",
+        "DigiPal88",
+        "TheBotWhisperer9",
+        "BotGenius77",
+        "RoboChamp2022",
+        "AI_Savant10",
+        "AutoAssistX5",
+        "BotWizard",
+        "CyberSage",
+        "TechGuruBot",
+        "GeniusAI",
+        "BotThinkTank",
+        "DigitalHelper",
+        "ArtificialBrain",
+        "AI_Buddy",
+        "AutoGenie",
+        "CyberCompanion",
+        "TechSavvyBot",
+        "GeniusMind",
+        "BotLogic",
+        "DigitalExpert",
+        "ArtificialMindset",
+        "AI_Mentor",
+        "AutoMind",
+        "CyberCoach",
+        "TechWizardBot",
+        "GeniusGenie",
+        "BotProdigy",
+        "DigitalBrain",
+        "ArtificialWhiz",
+        "AI_Ninja",
+        "AutoSherpa",
+        "CyberSensei",
+        "TechMasterBot",
+        "GeniusProdigy",
+        "BotBrainiac",
+        "DigitalSavant",
+        "ArtificialHelper",
+        "AI_Wisdom",
+        "AutoWhiz",
+        "CyberGuru",
+        "TechSageBot",
+        "GeniusSavvy",
+        "BotSavant",
+        "DigitalScribe",
+        "ArtificialSolutions",
+        "AI_Support"
+    ]
+    return random.choice(bot_handles)  # return a randomly selected bot name from the list
+
+
+def get_code_parts(part):
+    """
+    Extracts code parts from a given string using regex pattern matching.
+
+    Args:
+        part (str): The string to extract code parts from.
+
+    Returns:
+        list[str]: A list of strings containing the code parts found in the input string.
+
+    """
+    pattern = r"```(?:\w+\n)?([\s\S]+?)```"  # regex pattern to match code parts
+    code_parts = re.findall(pattern, part)  # extract code parts from input string using regex pattern matching
+    return code_parts  # return the list of code parts found in the input string
+
+
+def random_string(length=16):
+    """
+    Generates a random string of the specified length.
+
+    Args:
+        length (int, optional): The length of the generated string. Defaults to 16.
+
+    Returns:
+        str: The generated random string.
+
+    """
+    # Use list comprehension to generate a list of random letters from the ascii_letters string
+    # Join the list of letters into a single string
+    return ''.join([random.choice(string.ascii_letters) for x in range(length)])
+
+
+def split_code_blocks(input_string):
+    """
+    Splits a string at triple ticks ``` code blocks.
+
+    Args:
+        input_string (str): The string to split.
+
+    Returns:
+        list[str]: A list of strings containing the code blocks and the text outside of the code blocks.
+
+    """
+    # Regex pattern to find code blocks
+    pattern = r'(```(?:\w+\n)?(?:[\s\S]+?)```)'
+    return re.split(pattern, input_string)  # split the input string at the code blocks using the regex pattern
+
+
+def download_file(url, path):
+    """
+    Downloads a file from the specified URL and saves it to the specified path.
+
+    Args:
+        url (str): The URL of the file to download.
+        path (str): The path to save the downloaded file to.
+
+    Returns:
+        str: The path to the saved file.
+
+    """
+    urllib.request.urlretrieve(url, path)  # download the file from the URL and save it to the specified path
+    return path  # return the path to the saved file
+
+
+def unzip(path, output_path=None):
+    """
+    Extracts a zip file to a specified output directory.
+
+    Args:
+        path (Path): The path to the zip file to extract.
+        output_path (Path, optional): The directory to extract the contents to. If not specified, the contents will be
+            extracted to a new directory with the same name as the zip file in the same parent directory. Defaults to None.
+
+    Returns:
+        Path: The path to the directory containing the extracted contents.
+
+
+    """
+    output_path = output_path if output_path else path.parent / path.stem  # set default output path if not specified
+    with zipfile.ZipFile(path, 'r') as zip_ref:  # open the zip file
+        zip_ref.extractall(output_path)  # extract all files to output directory
+    return output_path  # return the path to the extracted directory
+
+
+def pip_location_to_path(pip_show: str):
+    location = None
+    for line in pip_show.split('\n'):
+        if line.startswith('Location'):
+            location = Path(line.replace('Location: ', ''))
+
+    return location
+def open_url(url):
+    webbrowser.open(url, new=0, autoraise=True)
+
+if __name__ == '__main__':
+    open_url('https://platform.openai.com/account/api-keys')
