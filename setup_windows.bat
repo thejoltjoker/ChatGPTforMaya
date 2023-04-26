719c6029817f29d99734266e847b6aac8ba2350e
Johannes <hello@thejoltjoker.com>
2023-04-26 10:48:57 +0200

    add contingency if Documents folder not exist

--- setup_windows.bat
+++ setup_windows.bat
@@ -5,7 +5,12 @@ set /P MAYA_VERSION=What Maya version are you using? (2021, 2022, etc):
 set MAYA_BIN_PATH=C:\Program Files\Autodesk\Maya%MAYA_VERSION%\bin\
 set VENV_PATH=%~dp0chatgpt4maya\src\venv
 set MODULE_PATH=%~dp0module
-set USER_MODULE_PATH=%USERPROFILE%\Documents\maya\%MAYA_VERSION%\modules
+IF EXIST "%USERPROFILE%\Documents" (
+    set USER_MODULE_PATH=%USERPROFILE%\Documents\maya\%MAYA_VERSION%\modules
+) ELSE (
+    set USER_MODULE_PATH=%MODULE_PATH%
+    setx MAYA_MODULE_PATH %MODULE_PATH%;%MAYA_MODULE_PATH%
+)
 set MODULE=%USER_MODULE_PATH%\chatGPTForMayaModule.mod
 
 
